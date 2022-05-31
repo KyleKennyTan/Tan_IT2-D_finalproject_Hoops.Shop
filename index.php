a6bc36af5a292d86cfeae1c24ee82885c666933d
Kyle Kenny Tan <kyriedrew11@gmail.com>
2022-05-31 12:57:56 +0800

    initial files

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <title>Supremmyy Sneakers</title>
+	    <link rel="stylesheet" href="css/style.css"> 
+    </head>
+    <body>
+        <div class="cover">
+            <div class="header">
+                 <img src="images/icons/logo.jpg">
+            </div>	
+            <div class="nav">
+               <ul>
+                   <li><a href="index.php" class="active">Home</a></li>
+                   <li><a href="aboutus.php">About Hoops</a></li>
+                   <li><a href="help.php">Help</a></li>
+                   <li><a href="contact.php">Contact</a></li>
+                   <li><a href="shoes.php">Bape Shoes</a></li>
+                   <li><a href="tshirt.php">Bape Shirts</a></li>
+                   <li><a href="supreme.php">Supreme Hoodies</a></li>
+                   <li><a href="supremestuffs.php">Supreme Stuffs</a></li>
+                   <li><a href="men.php">Men</a></li>
+                   <li><a href="women.php">Women</a></li>
+
+
+              </ul>
+              </ul>
+	    </div>
+        <div class="banner">
+            <img src="images/icons/Bape2.jpg">
+        </div>
+
+        <!--MAY AEDIT DINE-->
+        <div class="main">
+            <div class="left-side-bar">
+                <h2>Menu</h2>
+                <ul>
+                    <li><a href="membership.php">Membership Code</a></li> 
+                    <li><a href="gcashpayment.php">Gcash Payment Code</a></li>  					
+                    <li><a href="qnadiscount.php">QnA Discount</a></li>
+					<li><a href="studentdiscount.php">Student Discount</a></li>  
+                
+                </ul>
+            </div>
+
+                <div class="content">
+                    <h1 id="Purpose">Purpose</h1>
+                    <p>           Our purpose is to give every costomer an international vibe and brand with our products, also to look good, rich and to look like a celebrity in Hollywood because our products are from UK and US. What we have was the same as what celebrities are using.    </p>
+					
+					<h1 id="Goal">Goal</h1>
+                    <p>           To be an all in one international shop on every country.  </p>
+					
+                </div>
+                <div class="right-side-bar">
+                    <ul>
+                       <li><img src="images/icons/Supreme1.jpg"></li>
+                       <li><img src="images/icons/Supreme2.jpg"></li>
+                    </ul>
+                </div>
+        </div>
+            <div class="footer">
+            <p style="color: white">&copy; 2022 HOOPS.SHOP <br> Toronto, Canada <br> All Right Reserved   </p>
+                <a href="https://www.instagram.com/kennythegreat11/ "><img src="images/icons/insta.jpg"></a>
+                <a href="https://www.youtube.com/channel/UCZda-wRgoud-GyNTIGwTMsQ"><img src="images/icons/yt.png"></a>
+                
+        </div>
+    </body>
+</html>
+
+
+
+
+
+
+
+
